caae5f4c29fbab186d25c7d60c22a3c688c1accb
schmwong <79643071+schmwong@users.noreply.github.com>
2021-06-24 20:51:35 +0800

    Delete Exercise 1 directory

--- Exercise 1/ex1paul.js
+++
@@ -1,8 +0,0 @@
-//alert ('hello world'); 
-             
-function myFunction() {
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode("MYDigi");
-    node.appendChild(textnode);
-    document.getElementById("banklist").appendChild(node);
-}
